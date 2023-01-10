cc750a33272b16b6d3e7542143c3ae6f9a3dc005
Samantha-a-T <115659673+Samantha-a-T@users.noreply.github.com>
2023-01-10 14:21:09 -0700

    remove bootstrap and add moment

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let losAngelesId= document.querySelector("#los-angeles")
+let losAngelesDate = losAngelesId.querySelector(".date")
+let losAngelesTime = losAngelesId.querySelector(".time");
+losAngelesDate.innerHTML= "August 15th 2022"
+losAngelesTime.innerHTML = `1:49:15 <span class="time-indicator">AM</span>`;
